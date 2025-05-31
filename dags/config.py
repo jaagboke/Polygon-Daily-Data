8cf22ac5db5fc696b64539f38d8aa140748898c8
jaagboke <85943732+jaagboke@users.noreply.github.com>
2025-05-31 11:21:32 +0100

    Delete dags/config.py

--- dags/config.py
+++
@@ -1,5 +0,0 @@
-configurations = {
-    "account_name": "hvhistoricaldata",
-    "account_key": "VKdzWgGRLO8S6QTVAwtmUAHLIkVZfFZar9685UPjUNlMqx5VqAoZova0RcNxu6pVLoqqW16w0vf8+AStSyQE+A==",
-    "api_key": "3fR5pWYSKwOgkTLlby61T84jki1DuVvr"
-}
